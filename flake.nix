d4b850b9890269624d3dc48e2b3881be817e0b30
Florian Beeres <yuuki@protonmail.com>
2023-05-30 20:36:08 +0200

    add & use alejandra

--- flake.nix
+++ flake.nix
@@ -6,18 +6,22 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = import nixpkgs {
           inherit system;
         };
-      in
-      {
+      in {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             coreutils
             moreutils
+            alejandra
             jq
           ];
         };
